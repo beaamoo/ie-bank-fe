7ffe8b7a20dd4614ca99064fcfe71be792758638
beaamoo <beatrice.mossberg@gmail.com>
2023-10-15 01:25:41 +0200

    homepage update

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,6 +4,7 @@
       <div class="row">
         <div class="col-sm-12">
           <h1 class="title">Welcome to IE Bank</h1>
+          <p class="catch-phrase">Banking for a Better Tomorrow</p>
           <div class="button-container">
             <button class="btn btn-primary btn-lg" @click="$router.push('/accounts')">Go to Accounts</button>
           </div>
@@ -19,13 +20,50 @@
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: #f5f5f5;
+  background-image: url('/public/bank-bg.jpg');
+  background-size: cover;
+  background-position: center;
+  color: #fff;
+  overflow: hidden;
 }
 
 .title {
-  font-size: 3rem;
-  margin-bottom: 2rem;
+  font-size: 4rem;
   text-align: center;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  font-weight: 600;
+  animation: titleAnimation 3s ease-in-out 1.5s forwards;
+  transform: translateY(-100%);
+  margin-bottom: 2rem; /* Add margin at the bottom of the title */
+}
+
+.catch-phrase {
+  font-size: 1.5rem;
+  text-align: center;
+  font-weight: 400;
+  animation: catchPhraseAnimation 3s ease-in-out 1.5s forwards;
+  opacity: 0;
+  margin-top: 0.5rem; /* Add margin at the top of the catch phrase to move it lower */
+}
+
+@keyframes titleAnimation {
+  0% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes catchPhraseAnimation {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 .button-container {
@@ -33,6 +71,7 @@
   justify-content: center;
   align-items: center;
   height: 100%;
+  margin-top: 0.5rem;
 }
 
 .btn {
