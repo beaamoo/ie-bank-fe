89352c810ba120d05f50da63bd089d280d91117d
beaamoo <beatrice.mossberg@gmail.com>
2023-10-15 00:07:17 +0200

    fe fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Skull from '../components/Skull.vue'
-import AppAccounts from '../components/AppAccounts.vue'
-import HomePage from '../components/HomePage.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Skull from "../components/Skull.vue";
+import AppAccounts from "../components/AppAccounts.vue";
+import HomePage from "../components/HomePage.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: HomePage
+    path: "/",
+    name: "HomePage",
+    component: HomePage,
   },
   {
-    path: '/skull',
-    name: 'Skull',
-    component: Skull
+    path: "/skull",
+    name: "Skull",
+    component: Skull,
   },
   {
-    path: '/accounts',
-    name: 'AppAccounts',
-    component: AppAccounts
-  }
-]
+    path: "/accounts",
+    name: "AppAccounts",
+    component: AppAccounts,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
